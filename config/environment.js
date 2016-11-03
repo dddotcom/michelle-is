27280cce570a79d62f1c3efa8249c2606804a934
dddotcom <ddortega@bu.edu>
2016-11-03 11:02:15 -0700

    changing ENV.rootURL for production environment to from /michelle-is/ to /

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,8 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/michelle-is/';
+    // ENV.rootURL = '/michelle-is/';
+    ENV.rootURL = '/';
 
   }
 
