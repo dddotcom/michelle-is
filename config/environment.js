096305bb78c0c046063d12a0f495f2e115a62c22
dddotcom <dddotcom@gmail.com>
2017-01-03 22:49:49 -0500

    Add GA tracking

--- config/environment.js
+++ config/environment.js
@@ -20,7 +20,16 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+    metricsAdapters: [
+      {
+        name: 'GoogleAnalytics',
+        environments: ['production'],
+        config: {
+          id: 'UA-89782914-1'
+        }
+      }
+    ]
   };
 
   if (environment === 'development') {
